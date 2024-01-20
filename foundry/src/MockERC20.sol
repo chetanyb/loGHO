920fd235b0e9fe41e6677a9d8e45c46f3f73bbc4
SwineCoder101 <liamfaruq@gmail.com>
2024-01-20 12:04:54 +0000

    use-foundry

--- foundry/src/MockERC20.sol
+++ foundry/src/MockERC20.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
 
-import "openzeppelin/contracts/token/ERC20/ERC20.sol";
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 1000 * 10 ** decimals());
+    constructor(string memory _sym, string memory _lab) ERC20(_sym, _lab) {}
+
+    function mint(address recipient, uint256 amount) external {
+        _mint(recipient, amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 18;
     }
 }
